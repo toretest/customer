33288886011cd9de540ee090ccf85aa22854894f
tore gard <dragerot@gmail.com>
2023-03-12 13:02:24 +0100

    Initial commit

---
+++ src/main/java/net/toregard/customer/Customer.java
@@ -0,0 +1,5 @@
+package net.toregard.customer;
+
+public class Customer {
+
+}
---
+++ src/test/kotlin/net/toregard/customer/CustomerApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.toregard.customer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
