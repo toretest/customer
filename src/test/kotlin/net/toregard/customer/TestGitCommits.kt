e14cd9c18d64b5fe76e532adb1f3fe1342d6c53c
tore gard <dragerot@gmail.com>
2023-03-27 21:53:26 +0200

    Jira-123: Test commit revers

---
+++ src/test/kotlin/net/toregard/customer/TestGitCommits.kt
@@ -0,0 +1,12 @@
+package net.toregard.customer
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TestGitCommits {
+
+    @Test
+    fun hellotest(){
+       assertThat(true).isTrue();
+    }
+}
