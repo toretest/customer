bbcad514509baed8deb329315e95cd9533b4c656
tore gard <dragerot@gmail.com>
2023-03-27 22:32:43 +0200

    commit3-test

--- src/test/kotlin/net/toregard/customer/CustomerApplicationTests.kt
+++ src/test/kotlin/net/toregard/customer/CustomerApplicationTests.kt
@@ -1,5 +1,6 @@
 package net.toregard.customer
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,11 @@ class CustomerApplicationTests {
 
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	fun testRenameCommit(){
+		assertThat(true).isTrue();
 	}
 
 }
